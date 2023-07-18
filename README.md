65e88cc397bb1543f0466fc2bc22eea8318290b4
De Sean Ward <dward1711@gmail.com>
2023-07-18 13:32:50 -0500

    Basic visual aspect of game

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Mod-1 Main Project - Tic Tac Toe
+
+This is a creation of the classic 'Tic-Tac-Toe' game.
+
+The game was created using:
+- HTML
+- CSS (incl. Flexbox)
+- JavaScript
+
+This is a project to complete the first module 
+of my Software Engineering certification [@Per Scholas](https://www.perscholas.org)
+
+The purpose is to demonstrate my knowledge of:
+- Semantic HTML and application structure
+- Styling and appearance using CSS
+- Dynamic events and interactions utilizing JavaScript
+- Classes and Object Oriented Programming
+- DOM manipulation
---
+++ script.js
@@ -0,0 +1,58 @@
+class Game {
+	constructor() {
+		let numOfPlayers = 2;
+		const player1 = new Player('De Sean')
+		const player2 = new Player()
+	}
+    
+	createGame() {
+        const gameArea = document.querySelector('#gameArea');
+		const p1Score = document.querySelector('#p1Score');
+		const p2Score = document.querySelector('#p2Score');
+        const message = document.querySelector('#message')
+        
+        let player1won = 1;
+        let player2won = 0;
+
+		p1Score.textContent = player1won;
+		p2Score.textContent = player2won;
+		message.textContent = 'In-game messages goes here...'
+
+		const blocks = ['', '', '', '', '', '', '', '', ''];
+
+		function createBoard() {
+			blocks.forEach((_, idx) => {
+				const block = document.createElement('div');
+
+				block.classList.add('square');
+				block.id = idx + 1;
+
+				if (idx === 0 || idx === 1 || idx === 2)
+					block.style.borderTop = 'none';
+				if (idx === 0 || idx === 3 || idx === 6)
+					block.style.borderLeft = 'none';
+
+				if (idx === 2 || idx === 5 || idx === 8)
+					block.style.borderRight = 'none';
+
+				if (idx === 6 || idx === 7 || idx === 8)
+					block.style.borderBottom = 'none';
+
+				gameArea.append(block);
+			});
+		}
+
+		createBoard();
+	}
+}
+
+
+class Player {
+    constructor(name) {
+        this.name = name
+    }
+}
+
+// Create and start the game
+const game = new Game();
+game.createGame();
